4cf228b3efd74a1118be4e2b4cfc7705e99df8f2
admin <admin@DESKTOP-9DOCRM1>
2024-09-24 16:26:30 +0900

    0924수업내용

--- hongong/src/chap01/Hello.java
+++ hongong/src/chap01/Hello.java
@@ -4,7 +4,14 @@ public class Hello {
 
 	public static void main(String[] args) {
 		
-		System.out.println("ㅋㅋㄹㅃㅃ");
+		System.out.println("Hello java~");
+		
+		char cha1 = '나';
+		System.out.println(cha1);
+		
+		int int1 = '나';
+		System.out.println(int1);
+		
 
 	}
 
